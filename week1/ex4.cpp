a8c10f4dfa5c14998264dce73ac3f9276e32c91f
Tran Van Tan Khoi <trannguyentankhoi@gmail.com>
2025-03-20 09:27:20 +0700

    Added "week1/ex4.cpp"

---
+++ week1/ex4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+void towers_of_hanoi_solver(int n_discs, int from, int to, int aux) {
+    if (n_discs == 1) {
+        std::cout << "Disc " << n_discs << ": " << from << "->" << to << '\n';
+        return;
+    }
+    towers_of_hanoi_solver(n_discs - 1, from, aux, to);
+    std::cout << "Disc " << n_discs << ": " << from << "->" << to << '\n';
+    towers_of_hanoi_solver(n_discs - 1, aux, to, from);
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+    towers_of_hanoi_solver(n, 0, 2, 1);
+}
